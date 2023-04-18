bbda272c08177d98a050e2f13b0ba71547fba194
MehmetAkkaya <mehmetakkaya1990@gmail.com>
2023-04-18 14:21:18 +0300

    properties is changed

--- src/test/java/net/seamlessly/utility/ConfigurationReader.java
+++ src/test/java/net/seamlessly/utility/ConfigurationReader.java
@@ -7,10 +7,11 @@ import java.util.Properties;
 public class ConfigurationReader {
 
     private static Properties properties = new Properties();
+    static String path = System.getProperty("user.dir") + "/configuration.properties";
 
     static {
         try {
-            FileInputStream fis = new FileInputStream("configuration.properties");
+            FileInputStream fis = new FileInputStream(path);
             properties.load(fis);
             fis.close();
         } catch (IOException e) {
